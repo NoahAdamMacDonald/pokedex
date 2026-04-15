53e6f7e187c8f8250da6cc39884a275dd6f5efdb
Noah MacDonald <nmacdonald2113@gmail.com>
2026-04-15 16:40:26 -0400

    updated layout to allow for tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,39 @@
 import { Tabs } from "expo-router";
+import { Image } from "react-native";
 
 export default function TabsLayout() {
-    return (
-    <Tabs>
-        <Tabs.Screen
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#E3350D",
+        tabBarInactiveTintColor: "#999",
+        headerShown: false,
+      }}>
+      <Tabs.Screen
         name="index"
         options={{
-            title: "Pokédex",
+          title: "Pokédex",
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require("../../assets/images/pokedex.png")}
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
         }}
-        />
-        <Tabs.Screen
+      />
+
+      <Tabs.Screen
         name="favorites"
         options={{
-            title: "Favorites",
+          title: "Favorites",
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require("../../assets/images/star.png")}
+              style={{ width: 24, height: 24, tintColor: color }}
+            />
+          ),
         }}
-        />
+      />
     </Tabs>
-);
+  );
 }
